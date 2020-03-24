a21706acbb743e79fe9ac059e4e2df6a7ce58eea
daniel tea <daniel.l.tea@gmail.com>
2020-03-24 00:00:08 -0700

    add basic readmes

--- src/pages/readme.js
+++ src/pages/readme.js
@@ -6,6 +6,55 @@ export default () => (
   <Layout>
     <Content>
       <h1>readme</h1>
+      <h2>true facts</h2>
+      <ul>
+        <li>
+          <strong>birthday</strong>: july 10, 2018
+        </li>
+        <li>
+          <strong>breed</strong>: mini australian shepherd
+        </li>
+        <li>
+          <strong>color</strong>: blue merle
+        </li>
+      </ul>
+
+      <h2>i like</h2>
+      <ul>
+        <li>belly rubs</li>
+      </ul>
+
+      <h2>i don't like</h2>
+      <ul>
+        <li>belly rubs</li>
+      </ul>
+
+      <h2>i am not allowed to</h2>
+      <ul>
+        <li>go into the trash can in the bathroom</li>
+      </ul>
+
+      <h2>tricks i know</h2>
+      <ul>
+        <li>sit</li>
+      </ul>
+
+      <h2>tips</h2>
+      <ul>
+        <li>
+          Sometimes if I don't eat it's because I'm too excited about something.
+          I should probably play more until I’m hungry. Or maybe adding a little
+          water in my food makes it taste better
+        </li>
+        <li>
+          I am suspicious of some strangers. Dunno why. I might bark if a
+          stranger shows up in a place I don’t expect. Like my apartment!!!
+        </li>
+        <li>
+          If I’m whining a lot and I haven’t gone potty yet then I should
+          probably go potty
+        </li>
+      </ul>
     </Content>
   </Layout>
 )
